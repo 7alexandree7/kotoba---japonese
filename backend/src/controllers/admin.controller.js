ad178db76e65135b3005ab94f901bed1d619bdfd
alexandre <alexandre020602@gmail.com>
2026-06-11 22:17:07 -0300

    feat: implement user management features including fetching, updating, and deleting users

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,7 +1,86 @@
+import { User } from "../models/user.models.js";
+
 export const testAdminRoute = (req, res) => {
     return res.status(200).json({
         success: true,
         message: "access granted to admin route",
         data: req.user
     })
+}
+
+
+export const getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find()
+        return res.status(200).json({
+            success: true,
+            message: "All users fetched successfully",
+            data: users
+        });
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error occurred while fetching all users"
+        })
+    }
+}
+
+
+export const updateUserRole = async (req, res) => {
+    const { userId } = req.params;
+    const { role } = req.body;
+
+    if (!userId || !role) {
+        return res.status(400).json({
+            success: false,
+            message: "User ID and role are required"
+        })
+    }
+
+    try {
+        const user = await User.findByIdAndUpdate(userId, { role }, { new: true });
+
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found"
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "User role updated successfully",
+            data: user
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error occurred while updating user role"
+        })
+    }
+}
+
+
+export const deleteAnyUser = async (req, res) => {
+    const { userId } = req.params;
+
+    try {
+        const user = await User.findByIdAndDelete(userId);
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: "User not found",
+            })
+        }
+        return res.status(200).json({
+            success: true,
+            message: "User deleted successfully",
+            data: user
+        })
+    } catch (error) {
+        return res.status(500).json({
+            success: false,
+            message: "Error occurred while deleting user"
+        })
+    }
 }
--- backend/src/routes/admin/admin.routes.js
+++ backend/src/routes/admin/admin.routes.js
@@ -1,10 +1,13 @@
 import {Router} from "express";
 import { verifyToken } from "../../middlewares/verifyToken.js";
 import {isAdmin} from "../../middlewares/isAdmin.js";
-import { testAdminRoute } from "../../controllers/admin.controller.js";
+import { testAdminRoute, getAllUsers, updateUserRole, deleteAnyUser } from "../../controllers/admin.controller.js";
 
 const router = Router();
 
 router.get("/test-admin", verifyToken, isAdmin, testAdminRoute);
+router.get("/get-all-users", verifyToken, isAdmin, getAllUsers);
+router.put("/update-user-role/:userId", verifyToken, isAdmin, updateUserRole);
+router.delete("/delete-any-user/:userId", verifyToken, isAdmin, deleteAnyUser);
 
 export default router;
