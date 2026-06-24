a4f7b8d837fae30a925cd2f41d01bbd05df2f46f
alexandre <alexandre020602@gmail.com>
2026-06-24 17:37:37 -0300

    adding route get my profile

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,27 @@
 import { User } from "../models/user.model.js";
+import { userNotPassword } from "../utils/userNotPassword.js";
+
+
+export const getMyProfile = async (req, res) => {
+
+    try {
+        const user = await User.findById(req.user._id)
+        if (!user) {
+            return res.status(404).json({ message: "Usuário não encontrado." });
+        }
+        return res.status(200).json({
+            message: "Perfil do usuário encontrado com sucesso.",
+            success: true,
+            user: userNotPassword(user)
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Erro ao buscar o perfil do usuário.",
+            success: false,
+            error: error.message
+        });
+    }
+}
 
 export const deleteAccount = async (req, res) => {
 
--- backend/src/routes/user/user.routes.js
+++ backend/src/routes/user/user.routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import { verifyToken } from '../../middlewares/verifyToken.js';
-import { deleteAccount } from '../../controllers/user.controller.js';
+import { deleteAccount, getMyProfile } from '../../controllers/user.controller.js';
 const router = Router();
 
+
+router.get("/my-profile", verifyToken, getMyProfile);
 router.delete("/delete-account", verifyToken, deleteAccount);
 
 
