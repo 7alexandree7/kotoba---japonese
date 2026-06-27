9c56ff20def643ea0004fca53e0923fbb2c7920e
alexandre <alexandre020602@gmail.com>
2026-06-27 19:50:13 -0300

    feat: implement change password functionality; add changePassword controller and update user routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,5 +1,6 @@
 import { User } from "../models/user.model.js";
 import { userNotPassword } from "../utils/userNotPassword.js";
+import bcrypt from "bcryptjs";
 
 
 export const getMyProfile = async (req, res) => {
@@ -66,6 +67,52 @@ export const deleteAccount = async (req, res) => {
     } catch (error) {
         return res.status(500).json({
             message: "Erro ao excluir o usuário.",
+            success: false,
+            error: error.message
+        });
+    }
+}
+
+
+export const changePassword = async (req, res) => {
+
+    const { password, newPassword } = req.body;
+
+    try {
+        const user = await User.findById(req.user._id);
+
+        if (!user) {
+            return res.status(404).json({ message: "Usuário nao encontrado." });
+        }
+
+        const passwordMatch = await bcrypt.compare(password, user.password);
+        if (!passwordMatch) {
+            return res.status(400).json({
+                message: "Senha atual incorreta.",
+                success: false
+            });
+        };
+
+        const samePassword = await bcrypt.compare(newPassword, user.password);
+        if (samePassword) {
+            return res.status(400).json({
+                message: "A nova senha deve ser diferente da senha atual.",
+                success: false
+            });
+        };
+
+        const hashedPassword = await bcrypt.hash(newPassword, 10);
+        user.password = hashedPassword;
+        await user.save();
+
+        return res.status(200).json({
+            message: "Senha alterada com sucesso.",
+            success: true,
+        });
+    }
+    catch (error) {
+        return res.status(500).json({
+            message: "Erro ao alterar a senha.",
             success: false,
             error: error.message
         });
--- backend/src/routes/user/user.routes.js
+++ backend/src/routes/user/user.routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { verifyToken } from '../../middlewares/verifyToken.js';
-import { deleteAccount, getMyProfile, updateProfile } from '../../controllers/user.controller.js';
+import { deleteAccount, getMyProfile, updateProfile, changePassword } from '../../controllers/user.controller.js';
 const router = Router();
 
 
 router.get("/my-profile", verifyToken, getMyProfile);
 router.put("/update-profile", verifyToken, updateProfile);
 router.delete("/delete-account", verifyToken, deleteAccount);
+router.put("/change-password", verifyToken, changePassword);
 
 
 export default router;
