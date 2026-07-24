b36bff8b8a1912bb852fcb058f51be079b5814f4
alexandre <alexandre020602@gmail.com>
2026-07-24 12:30:19 -0300

    feat: implement createKanji functionality and update routes for kanji creation

---
+++ backend/src/controllers/kanji.controller.js
@@ -0,0 +1,29 @@
+import { Kanji } from "../models/kanji.models.js"
+
+
+export const createKanji = async (req, res) => {
+
+    try {
+        const newKanji = new Kanji({ ...req.body, user: req.user._id, nextReviewAt: new Date() });
+
+        if (!newKanji) {
+            return res.status(404).json({ message: "Kanji nao encontrado." });
+        }
+
+        await newKanji.save();
+
+        return res.status(201).json({
+            message: "Kanji criado com sucesso!",
+            success: true,
+            kanji: newKanji
+        })
+    }
+
+    catch (error) {
+        res.status(500).json({
+            message: "Erro ao criar o kanji.",
+            success: false,
+            error: error.message
+        });
+    }
+}
--- backend/src/routes/kanji/Kanji.routes.js
+++ backend/src/routes/kanji/Kanji.routes.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
+import { verifyToken } from "../../middlewares/verifyToken.js";
+import { validate } from "../../middlewares/validateUser.js";
+import { kanjiSchema } from "../../schema/kanji.schema.js";
+import { createKanji } from "../../controllers/kanji.controller.js";
 
 const router = Router();
 
 
-router.post("/create-")
+router.post("/create-kanji", verifyToken, validate(kanjiSchema), createKanji);
 
 
 export default router;
